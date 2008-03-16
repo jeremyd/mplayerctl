85c4f0501f2fbaff592b7bf27082d28d53972466
jeremy <jeremy@5be286fb-642c-0410-adc7-b8b26bb78e56>
2008-03-16 08:30:24 +0000

    seek program, for when in xwinwrap mode, get-status will report on signal strength
    git-svn-id: http://svn.rubyonlinux.org/mplayerctl@16 5be286fb-642c-0410-adc7-b8b26bb78e56

---
+++ get-status.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby
+require 'drb/drb'
+
+DVR = "/dev/dvb/adapter0/dvr0"
+SERVER_URI = "druby://localhost:8989"
+recording = DRbObject.new_with_uri(SERVER_URI)  
+puts recording.get_status
+
